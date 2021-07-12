b05f9ca7969d06da657fec0887848a54334f5007
Thiago Santos <thiago.santos@FOLLOWIZE.LOCAL>
2021-07-12 17:58:14 -0300

    ajuste de rotas

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RoleController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\SegmentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +22,16 @@ Route::group(['middleware' => ['guest:api']], function () {
 Route::group(['middleware' => ['auth:api']], function () {
     Route::get('/users/profile', [UserController::class, 'profile']);
     Route::apiResource('/users', UserController::class);
+
+    // Route::get('/origins/options', [UserController::class, 'options']);
+    // Route::apiResource('/origins', UserController::class);
+
+    // Route::get('/roles/options', [RoleController::class, 'options']);
+    // Route::apiResource('/roles', RoleController::class);
+
+    // Route::get('/segments/options', [SegmentController::class, 'options']);
+    // Route::apiResource('/segments', SegmentController::class);
+
+    // Route::get('/unrealized_sale_reason/options', [UserController::class, 'options']);
+    // Route::apiResource('/unrealized_sale_reason', UserController::class);
 });
